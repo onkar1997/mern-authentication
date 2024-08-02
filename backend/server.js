de44c04865d4b623254519f40b5ece0310bed253
onkarpawar <onkarpawar25@gmail.com>
2024-08-02 13:13:58 +0530

    code fixed 2

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+import path from 'path';
 import cookieParser from 'cookie-parser';
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
